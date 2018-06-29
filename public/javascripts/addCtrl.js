4484e163106da93f23bd951d063e0ec12c1fdbd0
gowtham06 <gkannan35@gmail.com>
2018-06-29 16:24:14 -0500

    Changes with angular modules

--- public/javascripts/addCtrl.js
+++ public/javascripts/addCtrl.js
@@ -6,27 +6,31 @@ addCtrl.controller('addCtrl',function($scope,$http,geolocation,gservice){
     var long=0;
     var radius=0;
 
-    $scope.formData.latitude=-119.180395633;
-    $scope.formData.longitude=34.244498898;
-    $scope.formData.radius=1000000;
+    // $scope.formData.latitude=-119.180395633;
+    // $scope.formData.longitude=34.244498898;
+    // $scope.formData.radius=1000000;
 
     $scope.findParked=function(){
         var queryData={
-            latitude:$scope.formData.latitude,
-            longitude:$scope.formData.longitude,
-            radius:$scope.formData.radius
+            latitude:parseFloat($scope.formData.latitude),
+            longitude:parseFloat($scope.formData.longitude),
+            radius:parseFloat($scope.formData.radius)
         };
+
+        gservice.refresh(queryData.latitude,queryData.longitude,queryData.radius);
         // console.log(queryData.stringify());
         // console.log(queryData)
-        $http.post('/getParkedCars',queryData).then(function(data){
-            // console.log(data.data);
-            $scope.formData.latitude=queryData.latitude;
-            $scope.formData.longitude=queryData.longitude;
-            $scope.formData.radius=queryData.radius;
-            gservice.refresh($scope.formData.latitude,$scope.formData.longitude);
-        }),function (data) {
-                console.log('Error:' +data);
-            };
+        // $http.post('/getParkedCars',queryData).then(function(data){
+        //     console.log(data);
+        //     $scope.formData.latitude=queryData.latitude;
+        //     $scope.formData.longitude=queryData.longitude;
+        //     $scope.formData.radius=queryData.radius;
+        //     gservice.refresh($scope.formData.latitude,$scope.formData.longitude);
+        // }),function (data) {
+        //         console.log('Error:' +data);
+        //     };
+
+
 
     };
 
--- public/javascripts/gservice.js
+++ public/javascripts/gservice.js
@@ -14,31 +14,50 @@ angular.module('gservice', [])
         var selectedLat = 39.50;
         var selectedLong = -98.35;
 
-        googleMapService.refresh=function(latitude,longitude){
+        // var selectedLat
+
+
+
+        googleMapService.refresh=function(latitude,longitude,radius=1000000){
+
             locations=[];
             selectedLat=latitude;
             selectedLong=longitude;
 
-            $http.post('/getParkedCars').then(function (response) {
-                locations=convertToMapPoints(response);
+            var queryData={
+                latitude:latitude,
+                longitude:longitude,
+                radius:radius
+            }
+            // console.log(queryData)
+            $http.post('/getParkedCars',queryData).then(function (response) {
+                // console.log(response);
+                locations=convertToMapPoints(response.data);
+                // console.log(locations.length);
                 initialize(latitude,longitude);
 
             }),function () {
 
             };
             };
+        // console.log(selectedLat,selectedLong);
 
         var convertToMapPoints=function(response){
+            // console.log(response.data);
+            console.log(response.length);
             var locations=[];
             for(var i=0;i<response.length;i++){
                 var user=response[i];
+                console.log(user)
                 var contentString='<p><b>Username</b>: ' + user.id+
                     '<br><b>Duration</b>: ' + user.duration +
+                    '<br><b>Latitude</b>: ' + user.coordinates[0] +
+                    '<br><b>Longitude</b>: ' + user.coordinates[1] +
                     '</p>';
-                console.log(contentString);
+                // console.log(contentString);
                 locations.push({
                     latlon:new google.maps.LatLng(user.coordinates[1],user.coordinates[0]),
-                    message:new google.maps.InforWindow({
+                    message:new google.maps.InfoWindow({
                         content:contentString,
                         maxWidth:320
                     }),
@@ -49,15 +68,17 @@ angular.module('gservice', [])
             return locations;
         };
     var initialize=function (latitude,longitude) {
-        var myLatLng={lat:selectedLat,lng:selectedLong};
+        // var myLatLng={lat:selectedLat,lng:selectedLong};
+        var myLatLng={lat:selectedLong,lng:selectedLat};
 
         if(!map){
             var map=new google.maps.Map(document.getElementById('map'),{
-                zoom:3,
+                zoom:18,
                 center:myLatLng
             });
         }
         locations.forEach(function (n,i) {
+            // console.log(n,i);
 
             var marker=new google.maps.Marker({
                 position:n.latlon,
@@ -66,23 +87,59 @@ angular.module('gservice', [])
                 icon:"http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
             });
 
+            // console.log(marker)
+            // console.log(n.latlon);
             google.maps.event.addListener(marker,'click',function (e) {
                 currentSelectedMarker=n;
                 n.message.open(map,marker);
             });
         });
-        var initialLocation=new google.maps.LatLng(latitude,longitude);
-        var marker=new google.maps.Marker({
-            position:initialLocation,
-            // animation:google.maps.Animation.Bounce,
-            map:map,
-            icon:'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+        // var initialLocation=new google.maps.LatLng(longitude,latitude);
+        // var marker=new google.maps.Marker({
+        //     position:initialLocation,
+        //     animation:google.maps.Animation.Bounce,
+        //     map:map,
+        //     icon:'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+        // });
+        // lastMarker=marker;
+        var selectedLocation=[];
+        var contentString=
+            '<p><b>Latitude</b>: ' + latitude +
+            '<br><b>Longitude</b>: ' + longitude +
+            '</p>';
+        // console.log(contentString);
+        selectedLocation.push({
+            latlon:new google.maps.LatLng(longitude,latitude),
+            message:new google.maps.InfoWindow({
+                content:contentString,
+                maxWidth:320
+
+            }),
+            id:"",
+            duration:""
         });
-        lastMarker=marker;
+        selectedLocation.forEach(function (n,i) {
+            // console.log(n,i);
+
+            var marker=new google.maps.Marker({
+                position:n.latlon,
+                map:map,
+                title:"You are here",
+                icon:"http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+            });
+
+            // console.log(marker)
+            // console.log(n.latlon);
+            google.maps.event.addListener(marker,'click',function (e) {
+                currentSelectedMarker=n;
+                n.message.open(map,marker);
+            });
+        });
+
 
     };
 
-    google.maps.event.addDomListner(window,'load',googleMapService.refresh(selectedLat,selectedLong));
+    google.maps.event.addDomListener(window,'load',googleMapService.refresh(selectedLat,selectedLong));
 
     return googleMapService;
 
